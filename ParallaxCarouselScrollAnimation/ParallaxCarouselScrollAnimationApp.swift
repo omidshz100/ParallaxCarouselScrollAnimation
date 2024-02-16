d7946a50e1ff09b668d515481285e192468da9c6
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2024-02-16 10:55:18 +0100

    Initial Commit

---
+++ ParallaxCarouselScrollAnimation/ParallaxCarouselScrollAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParallaxCarouselScrollAnimationApp.swift
+//  ParallaxCarouselScrollAnimation
+//
+//  Created by Omid Shojaeian Zanjani on 16/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ParallaxCarouselScrollAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
